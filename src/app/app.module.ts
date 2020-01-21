7daad175f17e573a512a2b2434f0b1389d3deaac
Phanice <maureezgaga@gmail.com>
2020-01-21 11:24:02 +0300

    dded component nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    ProfileComponent
+    ProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username:string;
+  private clientid = 'ee886f9b23b256796c5a';
+  private clientsecret = '4a744521e45df949c60d73caa4ed683d073ad736';
+  constructor(private http:HttpClient) {
+   console.log("service is ready!"); 
+   this.username = 'atienophanice'
+   }
+   getProfileInfo(){
+    return this.http.get("2" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+    .map(res => res); 
+   }
+getProfileRepos(){
+  return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+    .map(res => res); 
+}
+
+updateProfile(username:string){
+this.username = username;
+}
+
 }
